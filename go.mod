be2021c6eac70ad3a7514b383a959ebf6394a573
newmohib <@@mohib100@@>
2024-09-03 20:01:15 +0600

    added: Using chi for routing into routes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/newmohib/go-sample-web-app
 
 go 1.18
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
--- html-tamplating/cmd/web/routes.go
+++ html-tamplating/cmd/web/routes.go
@@ -3,15 +3,29 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	// "github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/newmohib/go-sample-web-app/html-tamplating/pkg/config"
 	"github.com/newmohib/go-sample-web-app/html-tamplating/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// routs create with pat
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	// create route with chi
+	mux := chi.NewRouter()
+	// middleware
+	mux.Use(middleware.Recoverer)
+
+	// router
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
 	return mux
 }
